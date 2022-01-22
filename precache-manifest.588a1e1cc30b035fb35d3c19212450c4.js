ba8c51afc454529b9ea560f46b087bbd8c0b951c
SakibHasanCse <sakibhasan9641@gmail.com>
2022-01-22 19:32:29 +0600

    Updates

---
+++ precache-manifest.588a1e1cc30b035fb35d3c19212450c4.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3848bcb20224a4216e6a79b14ac0efb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be267433cf135cf07e12",
+    "url": "/static/css/2.e6f51e71.chunk.css"
+  },
+  {
+    "revision": "c4125bd2bab67281b346",
+    "url": "/static/css/main.fda50071.chunk.css"
+  },
+  {
+    "revision": "be267433cf135cf07e12",
+    "url": "/static/js/2.0995d474.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0995d474.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4125bd2bab67281b346",
+    "url": "/static/js/main.40a6de18.chunk.js"
+  },
+  {
+    "revision": "74ca039aabce4e4a9a3c",
+    "url": "/static/js/runtime-main.d7e80684.js"
+  },
+  {
+    "revision": "1d87f972e712d484d7663f83c89abd1a",
+    "url": "/static/media/404-error.1d87f972.svg"
+  },
+  {
+    "revision": "46abce9502c4a676c5504dad3d4af27f",
+    "url": "/static/media/Resume of Sakib Hasan.46abce95.pdf"
+  },
+  {
+    "revision": "0d23c8d4a9bf073e8fab4b808bb9e6e4",
+    "url": "/static/media/api.0d23c8d4.svg"
+  },
+  {
+    "revision": "aab8bf94e5b7217d6726adcec184ad22",
+    "url": "/static/media/article-ph-1.aab8bf94.png"
+  },
+  {
+    "revision": "c0af511fbbbcfa60860b014b934c65be",
+    "url": "/static/media/coding.c0af511f.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "dcdd308f82a250fe38e2d17cd7d3b3e6",
+    "url": "/static/media/hamberger.dcdd308f.svg"
+  },
+  {
+    "revision": "4fdd3f145957b661932ebb8e08468ba0",
+    "url": "/static/media/programing.4fdd3f14.svg"
+  },
+  {
+    "revision": "96a24614549fc7c4564d4455d9b8fb55",
+    "url": "/static/media/sakib.96a24614.jpg"
+  },
+  {
+    "revision": "b565466eeb5d22c927d2cacf9502c90a",
+    "url": "/static/media/shopping-online.b565466e.svg"
+  },
+  {
+    "revision": "4851a1cedf396eb5f2eb230738fdee10",
+    "url": "/static/media/vagueness.4851a1ce.svg"
+  }
+]);
